4e57586d29ad8380d5ec80e15cc8e7bb7222c72a
NPC-Roman <ispacebobo@gmail.com>
2023-08-06 12:04:47 +0300

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+console.log(container);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class=".js-gallery-item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+createMarkup(galleryItems);
+console.log(createMarkup);
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+container.addEventListener("click", ImageClick);
+function ImageClick(evt) {
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  evt.preventDefault();
+  modalOpen(evt);
+  console.log(evt.target.dataset.source);
+}
+
+function modalOpen(evt) {
+  const originalSizeImage = evt.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${originalSizeImage}" width="1980" height="1024">`
+  );
+  instance.show();
+
+  galleryImage.addEventListener("keydown", onEscapeClick);
+
+  function onEscapeClick(evt) {
+    if (modalOpen && evt.key === "Escape") {
+      instance.close();
+      galleryImage.removeEventListener("keydown", onEscapeClick);
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,19 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li> <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a> </li>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionsDelay: 250 });
